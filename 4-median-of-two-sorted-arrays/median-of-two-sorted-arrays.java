6e742dad2cf8ac98fb51e74ce744d905729b922d
HariharanSP-13 <hariharansp1316@gmail.com>
2026-06-01 16:08:58 +0530

    Time: 8 ms (13.68%) | Memory: 49.3 MB (14.52%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
@@ -2,7 +2,7 @@
 class Solution {
   
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-       //syncnode
+       //test
         int nums3[]=new int[nums1.length+nums2.length];
          System.arraycopy(nums1, 0, nums3, 0, nums1.length);
         System.arraycopy(nums2, 0, nums3, nums1.length, nums2.length);
