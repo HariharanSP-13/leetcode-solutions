56a1b3f4014e104749b7db3df11926da22b85c25
HariharanSP-13 <hariharansp1316@gmail.com>
2026-06-09 14:05:33 +0530

    Time: 0 ms (100.00%) | Memory: 43.3 MB (59.52%) - LeetSync

---
+++ 66-plus-one/plus-one.java
@@ -0,0 +1,16 @@
+class Solution {
+    //test
+    public int[] plusOne(int[] digits) {
+        //syncnode
+        for(int i=digits.length-1;i>=0;i--){
+             if(digits[i] + 1 !=10){
+                digits[i]+=1;
+                return digits;
+            }
+        digits[i]=0;
+        }
+        int [] newDigits=new int[digits.length+1];
+        newDigits[0]= 1;
+        return newDigits;
+    }
+}
